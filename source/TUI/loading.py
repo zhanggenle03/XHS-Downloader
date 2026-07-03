e01aa4a1cfcb35d5a1223cc34c3be21eeab3ebfa
张亘乐 <120160744+zhanggenle03@users.noreply.github.com>
2026-07-03 14:44:57 +0800

    移除取消按钮，仅保留ESC结束任务

--- source/TUI/loading.py
+++ source/TUI/loading.py
@@ -2,7 +2,7 @@ from textual.app import ComposeResult
 from textual.binding import Binding
 from textual.containers import Grid
 from textual.screen import ModalScreen
-from textual.widgets import Button, Label, LoadingIndicator
+from textual.widgets import Label, LoadingIndicator
 
 from ..translation import _
 
@@ -18,13 +18,8 @@ class Loading(ModalScreen):
         yield Grid(
             Label(_("程序处理中...")),
             LoadingIndicator(),
-            Button(_("取消"), id="cancel", variant="error"),
             classes="loading",
         )
-
-    async def on_button_pressed(self, event: Button.Pressed):
-        if event.button.id == "cancel":
-            self.action_cancel_task()
 
     def action_cancel_task(self):
         self.app.APP.cancel_task()
